4b9e71436bd5069eb1124966e65354a7c530fab5
Felix Cervin <fexellofficial@gmail.com>
2026-04-13 10:50:31 +0200

    +_redirects in /public

--- .netlify/edge-functions/remix-server.mjs
+++ .netlify/edge-functions/remix-server.mjs
@@ -6,5 +6,5 @@
       generator: "@netlify/remix-edge-adapter@3.4.2",
       cache: "manual",
       path: "/*",
-      excludedPath: ["/.netlify/*","/.vite/*","/apple-touch-icon.png","/assets/*","/favicon-96x96.png","/favicon.ico","/favicon.svg","/images/*","/site.webmanifest","/web-app-manifest-192x192.png","/web-app-manifest-512x512.png"],
+      excludedPath: ["/.netlify/*","/.vite/*","/apple-touch-icon.png","/assets/*","/favicon-96x96.png","/favicon.ico","/favicon.svg","/images/*","/site.webmanifest","/web-app-manifest-192x192.png","/web-app-manifest-512x512.png","/_redirects"],
     };
